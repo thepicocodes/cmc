da7c78a4627c79a1f1e025fab6cad9fb1a7d8b4a
aviderambo <joelbaezjcp@gmail.com>
2024-12-18 11:04:11 -0400

    Minor change

--- theme/page-cmc.php
+++ theme/page-cmc.php
@@ -23,7 +23,7 @@ get_header();
                     Diseñamos muebles innovadores y duraderos con materiales exclusivos como el HDF, ideales para cocinas, baños y closets. Con sede en Bávaro-Punta Cana y tienda en Nagua, ofrecemos calidad y personalización en toda República Dominicana y el Caribe.
                 </p>
 
-                <a href="" class="rounded-lg w-max mt-7 p-4 font-bold text-white bg-green-modular">Cotiza con nosotros</a>
+                <a href="/contacto" class="rounded-lg w-max mt-7 p-4 font-bold text-white bg-green-modular">Cotiza con nosotros</a>
             </div>
         </div>
 
@@ -48,13 +48,13 @@ get_header();
                 <form action="/" method="GET" class="relative flex flex-col md:flex-row items-center gap-2.5">
                     <div class="relative w-full">
                         <input type="text" name="line" value="cmc" hidden>
-                        <input type="search" 
-                                name="s" 
+                        <input type="search"
+                                name="s"
                                 class="rounded-lg border w-full h-[52px] border-gray-1000 p-[14px] text-base"
                                 placeholder="¿Buscar entre mis productos?">
                         <button type="submit" class="absolute top-2/4 right-[17px] -translate-y-2/4 bg-white">
                             <svg xmlns="http://www.w3.org/2000/svg"
-                                class="w-4 h-4" 
+                                class="w-4 h-4"
                                 viewBox="0 0 512 512">
                                 <path
                                     d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
@@ -108,7 +108,7 @@ get_header();
                         </a>
                     </div>
                 </div>
-            <?php 
+            <?php
                 endwhile;
                 wp_reset_postdata();
             else:
